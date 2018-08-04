4ddd32b74f2dff3c5806ae857df2297ed2786f6e
Ben Asselmeier <benasselmeier@gmail.com>
2018-08-03 20:35:35 -0500

    Short break; implementing user class and beginning work on landing page.

---
+++ src/main/java/com/benasselmeier/redhawkrentals/models/User.java
@@ -0,0 +1,41 @@
+package com.benasselmeier.redhawkrentals.models;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @ManyToMany
+    private List<Equipment> rentals;
+
+    public void addItem(Equipment item) {
+        rentals.add(item);
+    }
+
+    public User() { }
+
+    //Getters and setters
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public List<Equipment> getRentals() { return rentals;}
+
+    public void setName(String name) { this.name = name; }
+}
